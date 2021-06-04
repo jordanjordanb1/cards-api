e154f7c109dda3c3d84f4c8dc8fef472790a2886
Jordan Barbosa <jordansbarbosa01@gmail.com>
2021-06-04 12:34:17 -0400

    feat: add cache

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/jordanjordanb1/cards-api
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.7.2
+	github.com/google/uuid v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/dgraph-io/ristretto"
 	"github.com/gin-gonic/gin"
 
 	"github.com/jordanjordanb1/cards-api/api/decks"
 )
 
+var Cache *ristretto.Cache
+
 func main() {
+	createCache()
+
 	// Initialize router
 	router := gin.Default()
 
@@ -17,4 +24,24 @@ func main() {
 	decks.RouterRegister(v1.Group("/decks"))
 
 	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+// Creates an in-memory cache
+func createCache() {
+	fmt.Printf("Creating ristretto cache...\n")
+
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		BufferItems: 64,      // number of keys per Get buffer.
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Ristretto cache initialized\n")
+
+	// Sets global
+	Cache = cache
 }
